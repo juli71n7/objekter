2e7791dd16fe93cd8025aab8d4472353e4389e52
Julie Gejel <juli71n7@stud.kea.dk>
2023-02-02 11:49:05 +0100

    obejkt øvelser

---
+++ template.js
@@ -0,0 +1,20 @@
+const superHelt = [
+  {
+    superkraft: "flyve",
+    flyvehastighed: "200%",
+    dragtfarve: "rød & grøn",
+    hader: "skurke",
+    superheltevenner: ["SuperKurt", "SuperHenry", "Superdog", "SuperMax"],
+    billede: "super.png",
+  },
+  {
+    superkraft: "løbe hurtigt",
+    løbehastighed: "200%",
+    dragtfarve: "blå & rød",
+    hader: "skurke",
+    superheltevenner: ["SuperKurt", "SuperHenry", "Superdog", "SuperMax"],
+    billede: "super2.jpg",
+  },
+];
+
+const skabelon = document.querySelector("#minSkabelon").content;
