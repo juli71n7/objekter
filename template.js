3c1e2ee7da657165685c73e89d9470ceeda23f2f
Julie Gejel <juli71n7@stud.kea.dk>
2023-02-02 12:05:02 +0100

    færdig

--- template.js
+++ template.js
@@ -18,3 +18,18 @@ const superHelt = [
 ];
 
 const skabelon = document.querySelector("#minSkabelon").content;
+
+superHelt.forEach(visHelt);
+
+function visHelt(helt) {
+  console.log(helt);
+  const klon = skabelon.cloneNode(true);
+  klon.querySelector("h1").textContent = helt.superkraft;
+  klon.querySelector("h2").textContent = helt.flyvehastighed;
+  klon.querySelector("h3").textContent = helt.dragtfarve;
+  klon.querySelector("p").textContent = helt.hader;
+  klon.querySelector("div").textContent = helt.superheltevenner;
+  klon.querySelector("img").src = helt.billede;
+
+  document.querySelector("main").appendChild(klon);
+}
